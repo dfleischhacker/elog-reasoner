8b10df0c1ac82d5661116d242473453fd9e6b952
mniepert@gmail.com <mniepert@gmail.com@8ccb4b08-4d42-4515-1e4d-e8e30666fa77>
2011-11-17 18:33:46 +0000

    performance improvements of the MIS sampler added timing for reasoning and sampling

--- elog/src/de/elog/misSampler/MisSampler.java
+++ elog/src/de/elog/misSampler/MisSampler.java
@@ -210,39 +210,48 @@ public class MisSampler {
 		//determine all incoherent classes in the ontology
 		Set<OWLClass> incoherentClasses = reasoner.getUnsatisfiableClasses().getEntitiesMinusBottom();
 		System.out.println("There are " + incoherentClasses.size() + " unsatisfiable classes.");
-
-		System.out.println("Computing minimial inconsistent subsets...");
-				
-		//iterate over all incoherent classes in the merged ontology
-		for (OWLClass incoherentClass : incoherentClasses) {
-			
-			System.out.println("Computing explanations for unsatisfiable class " + incoherentClass + "...");
-			
-			Set<Set<OWLAxiom>> exp = expGen.getUnsatisfiableExplanations(incoherentClass, numOfExplanations);
-			//iterate over all explanations found by the reasoner
-			for (Set<OWLAxiom> axiomSet : exp) {
-
-				//only keep the axioms that have a confidence value
-				axiomSet.retainAll(sampleAxioms);
-				System.out.println(axiomSet);
-				
-				for (OWLAxiom currentAxiom : axiomSet) {
-					//stores the degree of the axiom for statistics and convergence tests
-					degree.put(currentAxiom, new Integer(0));
-					
-					//remove axiom from ontology if it is the only one
-					//it will have probability 0!
-					if (axiomSet.size() <= 1) {
-						manager.removeAxiom(ontology2, currentAxiom);
-					}
-				}
-				
-				//add the set of axioms (a conflict) to the set of conflicts
-				conflictSet.add(axiomSet);
-			}
-		}
 		
-		System.out.println("...computation finished!");
+		System.out.println("Computing minimial inconsistent subsets...");
+		
+		final long startTimeReasoning = System.nanoTime();
+		final long endTimeReasoning;
+		try {
+	
+			//iterate over all incoherent classes in the merged ontology
+			for (OWLClass incoherentClass : incoherentClasses) {
+				
+				System.out.println("Computing explanations for unsatisfiable class " + incoherentClass + "...");
+				
+				Set<Set<OWLAxiom>> exp = expGen.getUnsatisfiableExplanations(incoherentClass, numOfExplanations);
+				//iterate over all explanations found by the reasoner
+				for (Set<OWLAxiom> axiomSet : exp) {
+	
+					//only keep the axioms that have a confidence value
+					axiomSet.retainAll(sampleAxioms);
+					System.out.println(axiomSet);
+					
+					for (OWLAxiom currentAxiom : axiomSet) {
+						//stores the degree of the axiom for statistics and convergence tests
+						degree.put(currentAxiom, new Integer(0));
+						
+						//remove axiom from ontology if it is the only one
+						//it will have probability 0!
+						if (axiomSet.size() <= 1) {
+							manager.removeAxiom(ontology2, currentAxiom);
+						}
+					}
+					
+					//add the set of axioms (a conflict) to the set of conflicts
+					conflictSet.add(axiomSet);
+				}
+			}
+
+		} finally {
+			  endTimeReasoning = System.nanoTime();
+			}
+		final long durationReasoning = endTimeReasoning - startTimeReasoning;
+		
+		System.out.println("...computation finished in " + String.format("%.1g%n", durationReasoning/1000000.0) + " milliseconds!");
 		System.out.println(conflictSet.size() + " MIS found.");
 		//System.out.println(conflictSet);
 		
@@ -282,125 +291,123 @@ public class MisSampler {
 		
 		System.out.println("Sampling process started...");
 		
-		//take numOfSamples samples
-		for (int s = 0; s < numOfSamples; s++) {
-			
-			//System.out.println("Sample number: " + s);
-			
-			//pick a random element (correspondence)
-			int item = new Random().nextInt(sampleAxioms.size());
-			OWLAxiom candidate = sampleAxioms.get(item);
-			
-			//check if the sample already contains the candidate
-			if (sample.contains(candidate)) {
+		final long startTimeSampling = System.nanoTime();
+		final long endTimeSampling;
+		try {
+
+			//take numOfSamples samples
+			for (int s = 0; s < numOfSamples; s++) {
 				
-				//remove the candidate with probability 1 / (1 + t)
-				double r = new Random().nextDouble();
-				if (r <= (1 / (1 + Math.exp(axiomConfidence.get(candidate))))) {
-					sample.remove(candidate);
-				}
+				//System.out.println("Sample number: " + s);
 				
-			} else {
-				//sample is does NOT contain candidate
-				//stores the unique conflict (edge in the HG) that the candidate causes
-				Set<OWLAxiom> uniqueConflict = new HashSet<OWLAxiom>();
+				//pick a random element (correspondence)
+				int item = new Random().nextInt(sampleAxioms.size());
+				OWLAxiom candidate = sampleAxioms.get(item);
 				
-				//add the element to the sample set
-				sample.add(candidate);
-				
-				//check whether the solution is actually an independent set
-				boolean independentSet = true;
-				//number of conflicts created by adding the candidate
-				int numOfConflicts = 0;
-				
-				//iterate over all conflict sets (edges in the HG)
-				for (Set<OWLAxiom> currentMIS : conflictSet) {
-									
-					//check if the current sample violates the independent set property
-					if (sample.containsAll(currentMIS)) {
-						independentSet = false;
-						numOfConflicts++;
-						
-						//if we have more than one conflict, we can stop here
-						if (numOfConflicts > 1) {
-							break;
-						}
-						
-						//uniqueConflict is the edge that would cause a unique conflict without the candidate
-						uniqueConflict = new HashSet<OWLAxiom>(currentMIS);
-						uniqueConflict.remove(candidate);
-					}
-				}
-									
-				//it is NOT an independent set, i.e., there's a conflict
-				if (!independentSet) {
+				//check if the sample already contains the candidate
+				if (sample.contains(candidate)) {
 					
-				//we have exactly one conflict
-					if (numOfConflicts == 1 && (uniqueConflict.size() > 0)) {
+					//remove the candidate with probability 1 / (1 + t)
+					double r = new Random().nextDouble();
+					if (r <= (1 / (1 + Math.exp(axiomConfidence.get(candidate))))) {
+						sample.remove(candidate);
+					}
+					
+				} else {
+					//sample is does NOT contain candidate
+					//stores the unique conflict (edge in the HG) that the candidate causes
+					Set<OWLAxiom> uniqueConflict = new HashSet<OWLAxiom>();
+					
+					//add the element to the sample set
+					sample.add(candidate);
+					
+					//check whether the solution is actually an independent set
+					boolean independentSet = true;
+					//number of conflicts created by adding the candidate
+					int numOfConflicts = 0;
+					
+					//iterate over all conflict sets (edges in the HG)
+					for (Set<OWLAxiom> currentMIS : conflictSet) {
+										
+						//check if the current sample violates the independent set property
+						if (sample.containsAll(currentMIS)) {
+							independentSet = false;
+							numOfConflicts++;
+							
+							//if we have more than one conflict, we can stop here
+							if (numOfConflicts > 1) {
+								break;
+							}
+							
+							//uniqueConflict is the edge that would cause a unique conflict without the candidate
+							uniqueConflict = new HashSet<OWLAxiom>(currentMIS);
+							uniqueConflict.remove(candidate);
+						}
+					}
+										
+					//it is NOT an independent set, i.e., there's a conflict
+					if (!independentSet) {
+						
+					//we have exactly one conflict
+						if (numOfConflicts == 1 && (uniqueConflict.size() > 0)) {
+							double r = new Random().nextDouble();
+							double tau = Math.exp(axiomConfidence.get(candidate));
+							double m = (double)max_edge_size;
+							double p = ((m-1)*tau) / (2*m*(tau+1));
+							
+							//with probability p choose an axiom in the conflict uniformly at random and remove it
+							if (r <= p) {
+							
+								//choose a random number between 0 and the size of the unique conflict minus 1
+								int n = new Random().nextInt(uniqueConflict.size());
+								//use an iterator and a variable (equal) to choose the axiom at random
+								Iterator<OWLAxiom> axm = uniqueConflict.iterator();
+								int equal = 0;
+								while ( axm.hasNext() ) {
+									OWLAxiom axiom = axm.next();
+									if (equal == n) {
+										//remove than axiom
+										sample.remove(axiom);
+										break;
+									}
+									equal++;
+								}
+							} else {
+								//we have that r > p and, thus, we remove the candidate (i.e., we leave the sample as it is)
+								sample.remove(candidate);
+							}						
+							
+						} else {
+							//we have more than one conflict caused by the candidate 
+							//and, thus, we remove the candidate (i.e., we leave the sample as it is)
+							sample.remove(candidate);
+						}
+	
+					} else {
+						//no conflict, with probability t / (1 + t) we add the candidate (i.e., do not remove the candidate)
 						double r = new Random().nextDouble();
 						double tau = Math.exp(axiomConfidence.get(candidate));
-						double m = (double)max_edge_size;
-						double p = ((m-1)*tau) / (2*m*(tau+1));
-						
-						//with probability p choose an axiom in the conflict uniformly at random and remove it
-						if (r <= p) {
-						
-							//choose a random number between 0 and the size of the unique conflict minus 1
-							int n = new Random().nextInt(uniqueConflict.size());
-							//use an iterator and a variable (equal) to choose the axiom at random
-							Iterator<OWLAxiom> axm = uniqueConflict.iterator();
-							int equal = 0;
-							while ( axm.hasNext() ) {
-								OWLAxiom axiom = axm.next();
-								if (equal == n) {
-									//remove than axiom
-									sample.remove(axiom);
-									break;
-								}
-								equal++;
-							}
-						} else {
-							//we have that r > p and, thus, we remove the candidate (i.e., we leave the sample as it is)
+						if (r > (tau / (1 + tau))) {
 							sample.remove(candidate);
-						}						
-						
-					} else {
-						//we have more than one conflict caused by the candidate 
-						//and, thus, we remove the candidate (i.e., we leave the sample as it is)
-						sample.remove(candidate);
+						}
 					}
-
-				} else {
-					//no conflict, with probability t / (1 + t) we add the candidate (i.e., do not remove the candidate)
-					double r = new Random().nextDouble();
-					double tau = Math.exp(axiomConfidence.get(candidate));
-					if (r > (tau / (1 + tau))) {
-						sample.remove(candidate);
-					}
+					
 				}
 				
-			}
-			
-			//update counts (after burn-in iterations)
-			if (s >= burn_in) {
-				
-				
-				for (OWLAxiom sAx : sample) {
-					count.put(sAx, new Integer(count.get(sAx)+1));
-				}
-								
-				/*for (int i = 0; i < sampleAxioms.size(); i++) {
-					
-					if (sample.contains(sampleAxioms.get(i))) {
-					
-						OWLAxiom currAx = sampleAxioms.get(i);
-						count.put(sampleAxioms.get(i), new Integer(count.get(currAx)+1));
+				//update counts (after burn-in iterations)
+				if (s >= burn_in) {
+					for (OWLAxiom sAx : sample) {
+						count.put(sAx, new Integer(count.get(sAx)+1));
 					}
-				}*/
+				}
 			}
-		}
 		
-		System.out.println("Sampling process finished.");
+		} finally {
+			endTimeSampling = System.nanoTime();
+		}
+		final long durationSampling = endTimeSampling - startTimeSampling;
+		
+		System.out.println("Sampling process finished in " + String.format("%.1g%n", durationSampling/1000000.0) + " milliseconds.");
 		
 		//compute the probabilities form the generated samples
 		for (int i = 0; i < sampleAxioms.size(); i++) {
@@ -444,7 +451,6 @@ public class MisSampler {
 			e.printStackTrace();
 		}
 	}
-		
 
 	
 	/**
